acf7d67414c7d84d1e650fd6e27c3d253ba247fa
hunterluker <hunterluker1992@gmail.com>
2019-04-04 13:39:56 -0600

    serve static files

--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
 require('dotenv').config();
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 const massive = require('massive');
 const pc = require('./products_controller');
 const sc = require('./stripe_controller');
+const path = require('path');
 
-let { SERVER_PORT, CONNECTION_STRING, } = process.env;
+let { SERVER_PORT, CONNECTION_STRING } = process.env;
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(express.static(`${__dirname}/../build`));
+
+app.use(bodyParser.json());
 
 massive(CONNECTION_STRING)
   .then(db => {
@@ -18,7 +21,6 @@ massive(CONNECTION_STRING)
   })
   .catch(err => console.log(err));
 
-
 // ENDPOINTS
 app.get('/api/products', pc.getAllProducts);
 
@@ -26,11 +28,13 @@ app.get('/api/products/:gender', pc.getAllProductsByGender);
 
 app.get('/api/product/:gender/:id', pc.getProduct);
 
-
 // CHECKOUT
 app.post('/api/checkout', sc.checkout);
 
-
 app.listen(SERVER_PORT, () => {
   console.log(`Listening on port: ${SERVER_PORT}`);
-})
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
